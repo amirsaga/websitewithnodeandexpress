b609da379c37c53e54a2cac48a8fd592c58c889c
Amir Shrestha <amir@homeloanexperts.com.au>
2016-12-28 23:41:51 +0545

    server with express

--- app/app.js
+++ app/app.js
@@ -1,17 +1,21 @@
-// var express = require('express');
-// var app = express();
+var express = require('express');
+var app = express();
 
-// var server = app.listen(8080, function() {
-//     console.log('Ready on port %d', server.address().port);
-// });
-
-var http = require('http');
-
-var myServer = http.createServer(function(request, response){
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write('Running on browser');
-  response.end();
+app.get('/', function(req, res) {
+  res.send('Run with express');
 });
 
-myServer.listen(3000);
-console.log('Ready to run');
+var server = app.listen(3000, function() {
+    console.log('Ready on port 3000');
+});
+
+// var http = require('http');
+
+// var myServer = http.createServer(function(request, response){
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.write('Running on browser');
+//   response.end();
+// });
+
+// myServer.listen(3000);
+// console.log('Ready to run');
