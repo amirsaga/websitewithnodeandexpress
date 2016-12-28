a424cbbb4d712e80715750320935ce17db9123c9
Amir Shrestha <amir@homeloanexperts.com.au>
2016-12-29 00:04:17 +0545

    read data from data.json and render

--- app/app.js
+++ app/app.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var app = express();
+var dataFile = require('./data/data.json');
+
+app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
-  res.send('Run with express');
+    var info = "";
+    dataFile.speakers.forEach(function(item) {
+        info += `
+            <li>
+                <h2>${item.name}</h2>
+                <p>${item.summary}</p>
+            </li>
+        `;
+
+    });
+    res.send(`
+        <h1>Run with express</h1>
+        ${info}
+        `);
 });
 
-var server = app.listen(3000, function() {
-    console.log('Ready on port 3000');
+var server = app.listen(app.get('port'), function() {
+    console.log('Ready on port ' + app.get('port'));
 });
 
 // var http = require('http');
