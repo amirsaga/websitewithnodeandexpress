73a18b06dd890257eda4105cf604fa805f11d8d4
Amir Shrestha <amir@homeloanexperts.com.au>
2016-12-30 10:47:42 +0545

    ejs view template local variable

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,12 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.send(`
-        <link rel="stylesheet" type="text/css" href="/css/style.css">
-        <h1>Run with express  nodemon</h1>
-        <img src="/images/misc/background.jpg" alt="express" style="height: 300px;">
-        <script src="/reload/reload.js"></script>
-        `);
+    var data = req.app.get('appData');
+    var pagePhotos = [];
+
+    data.speakers.forEach(function(item){
+        pagePhotos = pagePhotos.concat(item.artwork);
+    });
+
+    res.render('index', {
+        pageTitle: 'Home',
+        artwork: pagePhotos,
+        pageID: 'home'
+    });
 });
 
 module.exports = router;
