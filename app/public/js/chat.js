b6f955d5284cad23c41359dc58f94b1300198bf9
Amir Shrestha <amir@homeloanexperts.com.au>
2017-01-02 16:27:14 +0545

    asynchronus chat with socket.io

--- app/public/js/chat.js
+++ app/public/js/chat.js
@@ -1,24 +1,37 @@
-var chatForm = document.forms.chatForm;
+var socket = io();
+var chatUsername = document.querySelector('#chat-username');
+var chatMessage = document.querySelector('#chat-message');
+
+socket.on('connect', function() {
+    var chatForm = document.forms.chatForm;
 
 if (chatForm) {
-    var chatUsername = document.querySelector('#chat-username');
-    var chatMessage = document.querySelector('#chat-message');
 
     chatForm.addEventListener('submit', function(e) {
         e.preventDefault();
-        showMessage({
+        socket.emit('postMessage',{
             username: chatUsername.value,
             message: chatMessage.value
         });
         chatMessage.value = '';
         chatMessage.focus();
-    });
-}
+    }); //chatForm event
+
+    socket.on('updateMessages', function(data) {
+        showMessage(data);
+        }); //updateMessages
+    } //chatform
+});
 
 function showMessage(data) {
     var chatDisplay = document.querySelector('.chat-display');
     var newMessage = document.createElement('p');
-    newMessage.className = 'bg-success chat-text';
+
+    if (chatUsername.value == data.username) {
+        newMessage.className = 'bg-success chat-text';
+    } else {
+        newMessage.className = 'bg-info text-warning chat-text';    
+    }
     newMessage.innerHTML = '<strong>' + data.username + '</strong>: ' + data.message;
     chatDisplay.insertBefore(newMessage, chatDisplay.firstChild);
 }
